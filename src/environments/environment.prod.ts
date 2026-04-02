8557c4e82d40f41a2ea738ab97a120e3917ed897
rphsmn <ralphsamsonidsc@gmail.com>
2026-04-02 16:43:02 +0800

    firebase api errors fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    /* Same keys go here later for the live site */
+    apiKey: "AIzaSyDhvTtu2a_CC3DdkIfA49qJWr5-cYpKvU0",
+    authDomain: "cor-logic-hris.firebaseapp.com",
+    projectId: "cor-logic-hris",
+    storageBucket: "cor-logic-hris.firebasestorage.app",
+    messagingSenderId: "611895985104",
+    appId: "1:611895985104:web:13747d65663a005a61afd5"
   }
 };
