5f6da8fad0e7026c51dcbc1f3a698a05cafc082d
rphsmn <ralphsamsonidsc@gmail.com>
2026-04-07 13:57:23 +1000

    package json for netlify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDhvTtu2a_CC3DdkIfA49qJWr5-cYpKvU0",
-    authDomain: "cor-logic-hris.firebaseapp.com",
-    projectId: "cor-logic-hris",
-    storageBucket: "cor-logic-hris.firebasestorage.app",
-    messagingSenderId: "611895985104",
-    appId: "1:611895985104:web:13747d65663a005a61afd5"
+ apiKey: "AIzaSyCpZ1g05w5CJPmYMJhsHVAqwglnPdt7D4E",
+  authDomain: "corlogic-hris-dev.firebaseapp.com",
+  projectId: "corlogic-hris-dev",
+  storageBucket: "corlogic-hris-dev.firebasestorage.app",
+  messagingSenderId: "1036476709128",
+  appId: "1:1036476709128:web:8e60b449cb4f37a34554bd",
+  measurementId: "G-4H1FZ2KDT9"
   }
 };
